a32070b3ca7f63ca5cb752621da08bf00216a8e5
7 <ruuuziiir@gmail.com>
2021-03-28 14:38:43 +0800

    third-2

--- CoffeeQA/ResultViewController.swift
+++ CoffeeQA/ResultViewController.swift
@@ -12,12 +12,14 @@ class ResultViewController: UIViewController {
     
     @IBOutlet weak var finalScoreLabel: UILabel!
     
+    // 要在目的地先宣告變數，才能將前一個頁面的值傳入
     var finalScore: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         finalScoreLabel.text = finalScore
+        
     }
     
     @IBAction func jump(_ sender: Any) {
